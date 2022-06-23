29b02e28dc6a38a25c52c595ade283b1ca8b034d
Brian Ferreira <101906190+EoBrian@users.noreply.github.com>
2022-06-23 11:40:45 -0300

    implemento da função que gera as senhas

--- components/Password-Generator.js
+++ components/Password-Generator.js
@@ -1,23 +1,42 @@
-var choice_number = document.getElementById('input-number')
-var password_generated = document.getElementById('password-gen')
-var button = document.getElementById('button-press')
-
-password = []
-
-list_lowercase = 'abcdefghijklmnopqrstuvwxyz'
-list_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-var number = Math.random() * 25
+var 
 
 
+var list_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+var list_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var list_numbers_pontuation = '123456789!@#$%&*-_'
 
 
-button.addEventListener('click', function(){
+function choice_list(list){
+    /*
+    pega um item aleatório do Array
+    */
+    var name_var = Math.floor(Math.random() * list.length)
+    var choice = list[name_var]
+    return choice
+}
 
-    for (let i = 0; i < choice_number.value; i++){
-        for (let cases in list_lowercase) {
-           
+
+function PasswordGenerator(number_choice=8){
+    /*
+    Aqui gera a senha em si
+    */
+    var password = new Array() //armazena a senha gerada
+    
+    for (let i = 0; i < number_choice; i++){
+
+        var rng = Math.floor(Math.random() * 4) //para que crie senhas um pouco memoráveis
+        
+        if (rng == 0){
+            password += choice_list(list_lowercase)
+        }
+        
+       else if (rng == 1 || rng == 2){
+            password += choice_list(list_uppercase)
+        }
+        
+        else{
+            password += choice_list(list_numbers_pontuation)
         }
     }
-    password_generated.value = 'eric'
-})
+    return password
+}
