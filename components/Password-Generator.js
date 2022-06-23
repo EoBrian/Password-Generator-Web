139f5675a55762dae02f9135ac18ee1f76a80eb9
Brian Ferreira <101906190+EoBrian@users.noreply.github.com>
2022-06-23 11:03:04 -0300

    working

---
+++ components/Password-Generator.js
@@ -0,0 +1,23 @@
+var choice_number = document.getElementById('input-number')
+var password_generated = document.getElementById('password-gen')
+var button = document.getElementById('button-press')
+
+password = []
+
+list_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+list_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+var number = Math.random() * 25
+
+
+
+
+button.addEventListener('click', function(){
+
+    for (let i = 0; i < choice_number.value; i++){
+        for (let cases in list_lowercase) {
+           
+        }
+    }
+    password_generated.value = 'eric'
+})
