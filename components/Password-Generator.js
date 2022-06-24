3602ed7f519ff40b77815914a38622cedbb2b3fe
Brian Ferreira <101906190+EoBrian@users.noreply.github.com>
2022-06-24 10:23:03 -0300

    alterando o design

--- components/Password-Generator.js
+++ components/Password-Generator.js
@@ -4,6 +4,7 @@ var buttonEl = document.getElementById('button-press')
 var passwordEl = document.getElementById('password-gen')
 
 
+
 var list_lowercase = 'abcdefghijklmnopqrstuvwxyz'
 var list_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var list_numbers_pontuation = `123456789!&@#$%*-_`
@@ -23,6 +24,21 @@ function PasswordGenerator(){
     /*
     Aqui gera a senha em si
     */
+    
+    var progress_barEl = document.getElementById('progress-bar_')
+    
+    if (numberEl.value <= 7){
+        progress_barEl.style.backgroundColor = '#f00'
+        progress_barEl.style.width = '20%'
+
+    }else if (numberEl.value > 7 && numberEl.value < 10){
+        progress_barEl.style.backgroundColor = '#f0d769'
+        progress_barEl.style.width = '70%'
+
+    }else{
+        progress_barEl.style.backgroundColor = '#663399'
+        progress_barEl.style.width = '120%'
+    }
 
     var password = new Array()
     
